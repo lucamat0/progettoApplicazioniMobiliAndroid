daad98e1434a434b8c774bb51a0f7354761ebf77
Matteo Busto <20030663@studenti.uniupo.it>
2023-02-21 13:19:05 +0000

    update main kt

--- app/src/main/java/it/uniupo/oggettiusati/MainActivity.kt
+++ app/src/main/java/it/uniupo/oggettiusati/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //test modifica
+        //test modifica 2
     }
 }
