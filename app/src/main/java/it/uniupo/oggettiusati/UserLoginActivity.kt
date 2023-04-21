eb4d808def41a2134577b7178f107cd8397281b5
Matteo Busto <20030663@studenti.uniupo.it>
2023-04-21 17:24:53 +0200

    update identifier myAnnunci to myAnnunciHome

--- app/src/main/java/it/uniupo/oggettiusati/UserLoginActivity.kt
+++ app/src/main/java/it/uniupo/oggettiusati/UserLoginActivity.kt
@@ -108,7 +108,7 @@ open class UserLoginActivity : AppCompatActivity() {
         //This loop will create as many Views as documents containing
         //the image with title and price of object
         for (key in myAnnunciHome.keys){
-            data.add(ItemsViewModel(myAnnunciHome[key]?.annuncioId, R.drawable.ic_launcher_background, "${myAnnunci[key]?.getTitolo()}", myAnnunci[key]?.getPrezzo(), auth.currentUser?.email/*, myAnnunci[key]?.getNTel()*/) )
+            data.add(ItemsViewModel(myAnnunciHome[key]?.annuncioId, R.drawable.ic_launcher_background, "${myAnnunciHome[key]?.getTitolo()}", myAnnunciHome[key]?.getPrezzo(), auth.currentUser?.email/*, myAnnunciHome[key]?.getNTel()*/) )
         }
 
         //this will pass the ArrayList to our Adapter
